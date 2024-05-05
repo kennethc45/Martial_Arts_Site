dbfa26625706f41d30a13f6ee527fa815753bc5e
KennethCadungog-Trinity University <112190731+KennethCadungog@users.noreply.github.com>
2024-05-04 22:27:16 -0500

    Started building web app

--- technique_library/src/main.rs
+++ technique_library/src/main.rs
@@ -1,11 +1,78 @@
 // use technique_library::technique_catalog::*;
+mod handler;
+mod model;
+mod route;
+mod schema;
 
-fn main() {
-    // let mut techniques: TechniqueLibrary = TechniqueLibrary::new();
-    // let roundhouse = Technique::new(
-    //     "Roundhouse Kick".to_string(),
-    //     TechniqueCategory::Strike(StrikeCategory::Kick),
-    //     "A powerful and dynamic kick that involves rotating the hips and torso to deliver a devestating strike.".to_string()
-    // );
+use std::sync::Arc;
+
+use axum::http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
+};
+use axum::response::{IntoResponse, Html};
+use axum::Json;
+use axum::{routing::{get, post}, Router, extract::{State, Path}};
+use dotenv::dotenv;
+use route::create_router;
+use tower_http::cors::CorsLayer;
+
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+use serde::{Deserialize, Serialize};
+use tokio::net::TcpListener;
+
+pub struct AppState {
+    db: Pool<Postgres>,
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("Connection to the database is successful");
+            pool
+        }
+        Err(err) => {
+            println!("Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+
+    let address_num = "127.0.0.1:3000";
+
+    let server_address = std::env::var("SERVER_ADRESS")
+        .unwrap_or(address_num.to_owned());
+
+    let listener = TcpListener::bind(server_address)
+        .await
+        .expect("Could not create TCP Listener");
+
+    println!("Listening on {}", listener.local_addr().unwrap());
+
+    let app_state = Arc::new(AppState { db: pool.clone() });
+
+    // Defined endpoints 
+    let app = create_router(Arc::new(AppState { db: pool.clone() })).layer(cors);
+
+    // Launches the local server
+    axum::serve(listener, app)
+        .await
+        .expect("Error serving application")
 
 }
+
+
--- technique_library/target/debug/libtechnique_library.d
+++ technique_library/target/debug/libtechnique_library.d
@@ -1,1 +1,1 @@
-/Users/kennethcadungog/technique-library/technique_library/target/debug/libtechnique_library.rlib: /Users/kennethcadungog/technique-library/technique_library/src/lib.rs
+/Users/kennethcadungog/Martial_Arts_Site/technique_library/target/debug/libtechnique_library.rlib: /Users/kennethcadungog/Martial_Arts_Site/technique_library/src/lib.rs
